964438c6d87f8d28e410b70ea65ee149f1780d4a
Ellinor <ellinor.stenhaug@gmail.com>
2019-04-05 10:18:13 +0200

    ProductList, product, sucess, order

---
+++ productList.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    productList.init();
+})
+
+let productList = (function () {
+    let init = function () {
+        renderProducts();
+    }
+
+    function renderProducts () {
+        
+    }
+
+    return {
+        init: init
+    }
+})();
+
